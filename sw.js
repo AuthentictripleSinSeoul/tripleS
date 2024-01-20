d3fa516b163b2f7ddd605c4020d573dcbffd2ef9
tripleSAuthenticSeoul <157200338+tripleSAuthenticSeoul@users.noreply.github.com>
2024-01-21 05:49:30 +0900

    Update sw.js
    offline 대응 2

--- sw.js
+++ sw.js
@@ -1,20 +1,3 @@
-// sw.js
-
-const cacheName = 'Cheering-Guide-Cache-v2'; // Update cacheName to trigger an update
-const cacheAssets = [
-    '/tripleS/index.html',
-    '/tripleS/styles.css',
-    '/tripleS/script.js',
-    '/tripleS/generation.png',
-    '/tripleS/rising.png',
-    '/tripleS/cherry-talk.png',
-    '/tripleS/touch.png',
-    '/tripleS/girls-capitalism.png',
-    '/tripleS/invincible.png',
-    '/tripleS/just-do-it.png',
-    '/tripleS/1.mp4'
-];
-
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName).then(cache => {
@@ -23,17 +6,9 @@ self.addEventListener('install', event => {
     );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-// Cache update logic
 self.addEventListener('activate', event => {
     event.waitUntil(
+        // 현재 캐시 버전 이외의 다른 버전의 캐시를 모두 삭제
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
@@ -42,6 +17,16 @@ self.addEventListener('activate', event => {
                     }
                 })
             );
+        })
+    );
+    // 강제로 서비스 워커를 활성화
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
