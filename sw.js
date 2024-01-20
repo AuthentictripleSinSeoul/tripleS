3816ea98ceaec9fb0c5f0296f916f027c13897f9
tripleSAuthenticSeoul <157200338+tripleSAuthenticSeoul@users.noreply.github.com>
2024-01-21 05:53:14 +0900

    Update sw.js
    of

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+const cacheName = 'Cheering-Guide-Cache-v1';
+const cacheAssets = [
+    '/tripleS/index.html',
+    '/tripleS/styles.css',
+    '/tripleS/script.js',
+    '/tripleS/generation.png',
+    '/tripleS/rising.png',
+    '/tripleS/cherry-talk.png',
+    '/tripleS/touch.png',
+    '/tripleS/girls-capitalism.png',
+    '/tripleS/invincible.png',
+    '/tripleS/just-do-it.png',
+    '/tripleS/1.mp4'
+];
+
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName).then(cache => {
